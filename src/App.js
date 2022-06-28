9ead9153508ce88bdb51d5bd0298cd39fe63b47c
Kweku Bright <kweku01bright@gmail.com>
2022-06-28 14:19:26 +0100

    Remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
