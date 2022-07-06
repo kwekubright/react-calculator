ceda95d6124d2db87aece065db14723449a56dc5
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-06 15:16:24 +0100

    Add test for operate.js

---
+++ src/components/__tests__/calculate.test.js
@@ -0,0 +1,19 @@
+import calculate from "../../logics/calculate";
+
+describe("calculate", () => {
+  test("should return {total: 0, next: null, operation: null} when buttonName is AC", () => {
+    expect(calculate({ total: 0, next: null, operation: null }, "AC")).toEqual({
+      total: 0,
+      next: null,
+      operation: null,
+    });
+  });
+
+  test("should return {total: 7, next: null, operation: null} when buttonName is 3+4", () => {
+    expect(calculate({ total: "3", next: "4", operation: "+" }, "=")).toEqual({
+      total: "7",
+      next: null,
+      operation: null,
+    });
+  });
+});
