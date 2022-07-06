919af043db16bb657b46e43b2f45bb4f3f875ed4
Kweku Bright <kweku01bright@gmail.com>
2022-07-06 13:58:13 +0100

    Setup testing

---
+++ __tests__/calculate.test.js
@@ -0,0 +1,11 @@
+import calculate from "../src/logics/calculate";
+
+describe("calculate", () => { 
+  test("should return {total: 0, next: null, operation: null} when buttonName is AC", () => {
+    expect(calculate({ total: 0, next: null, operation: null }, "AC")).toEqual({ total: 0, next: null, operation: null });
+  });
+
+  test("should return {total: 7, next: null, operation: null} when buttonName is 3+4", () => {
+    expect(calculate({ total: 0, next: null, operation: null }, "3+4")).toEqual({ total: 7, next: null, operation: null });
+  });
+});
