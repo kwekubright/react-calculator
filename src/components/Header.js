95f657135f4a3bc006f3df2f2fde77970b5713e2
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-07 10:58:31 +0100

    Fix the root route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
 
 const Header = () => (
   <header className="container header flex flexJustifySpaceBetween flexAlignCenter">
     <div className="header-logo">
-      <Link to="/home"><h1 className="logo">Math Magicians</h1></Link>
+      <Link to="/">
+        <h1 className="logo">Math Magicians</h1>
+      </Link>
     </div>
     <div className="header-nav flex">
-      <Link to="/home" className="headerNavLink">
-        {' '}
-        {'Home | '}
+      <Link to="/" className="headerNavLink">
+        {" "}
+        {"Home | "}
       </Link>
-      <Link to="/calculator" className="headerNavLink">{' Calculator | '}</Link>
-      <Link to="/quotes" className="headerNavLink">{' Quote'}</Link>
+      <Link to="/calculator" className="headerNavLink">
+        {" Calculator | "}
+      </Link>
+      <Link to="/quotes" className="headerNavLink">
+        {" Quote"}
+      </Link>
     </div>
   </header>
 );
