a959d74f029de4dca0c2f7c33b9e0cf1c51bcb9c
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-07 11:02:07 +0100

    Fix test for the Header component

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,17 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from '../Header';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "../Header";
 
-describe('Header component test', () => {
-  test('render the Header component', () => {
-    const header = render(<Router><Header /></Router>);
+describe("Header component test", () => {
+  test("render the Header component", () => {
+    const header = render(
+      <Router>
+        <Header />
+      </Router>
+    );
     expect(header).toMatchSnapshot();
   });
 
-  test('check if the Header component rendered by checking if the logo exist in the DOM', () => {
-    render(<Router><Header /></Router>);
-    const logoElement = screen.getByText('Math Magicians', { exact: false });
+  test("check if the Header component rendered by checking if the logo exist in the DOM", () => {
+    render(
+      <Router>
+        <Header />
+      </Router>
+    );
+    const logoElement = screen.getByText("Math Magicians", { exact: false });
     expect(logoElement).toBeInTheDocument();
   });
 });
